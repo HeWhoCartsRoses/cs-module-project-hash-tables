e5bc8fe3bfcc4aece6ddf77a0219a85d8bb0fee3
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-07-09 13:53:48 -0600

    getting her done

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -5,11 +5,31 @@ f(a) + f(b) = f(c) - f(d)
 
 #q = set(range(1, 10))
 #q = set(range(1, 200))
+from itertools import permutations
 q = (1, 3, 4, 7, 12)
 
 
 def f(x):
     return x * 4 + 6
 
+
 # Your code here
 
+
+def test(var):
+    q = []
+    for i in var:
+        q.append(f(i))
+    x = list(permutations(q))
+    y = []
+    for l in x:
+        z = list(l)
+        add = z[0]+z[1]
+        sub = z[2]-z[3]
+        if add == sub:
+            y.append(z)
+    return y
+
+
+if __name__ == "__main__":
+    print(test(q))
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,25 @@
+from collections import Counter
+
+
 def word_count(s):
     # Your code here
-
+    special = '":;,.-+=/\|[]{}()*^&'
+    new = s
+    for i in special:
+        new = new.replace(i, '')
+    if len(new) == 0:
+        return {}
+    new = new.lower()
+    new = new.split()
+    new = Counter(new)
+    return new
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
+    print(
+        word_count("Hello    hello"))
